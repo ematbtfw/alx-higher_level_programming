d5b52996a435d2d93986eb7c221d59a314444744
ematbtfw <amangealtekeba@gmail.com>
2022-09-28 10:08:18 -0700

    task12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+# 14-pascal_triangle.py
+"""Defines a Pascal's Triangle function."""
+
+
 def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
     if n <= 0:
         return []
-    pascal = []
-    row = []
-    for i in range(n):
-        row = [1]
-        if i > 0:
-            for j in range(i):
-                row.append(sum(pascal[-1][j:j+2]))
-        pascal.append(row)
-    return pascal
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
