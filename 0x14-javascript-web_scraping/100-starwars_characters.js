6211ee984738063ed4d1943c309968cc2432d9e3
ematbtfw <amanusatekeba@gmail.com>
2023-01-17 12:01:43 +0000

    task7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+let movieDict = {};
+let characterDict = {};
+let i = 0;
 request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+  if (error) {
+    console.log(error);
+  } else {
+    movieDict = JSON.parse(body);
+    for (i = 0; i < movieDict.characters.length; i++) {
+      request(movieDict.characters[i], function (error, request, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          characterDict = JSON.parse(body);
+          console.log(characterDict.name);
         }
       });
-    });
+    }
   }
 });
